5e74f37d0b068f2b260990653478ee88da149fec
aliterm <me@situsali.com>
2023-10-13 00:33:28 +0700

    feat: db seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,35 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  before_validation :ensure_has_a_name
+
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable,
+         :confirmable,
+         :lockable,
+         :timeoutable
+
+  enum role: %i[customer admin]
+
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :email, presence: true, format: { with: /.+@.+\.{1}.{2,}/ }
+
+  validates :password,
+            length: {
+              in: 6..128
+            },
+            if: lambda { self.password.present? }
+  validates_confirmation_of :password, if: lambda { self.password.present? }
+
+  private
+
+  def ensure_has_a_name
+    return if self.email.blank?
+
+    self.name =
+      "#{self.email[/^[^@]+/].humanize.gsub(/[^a-zA-Z]/, "")}" if name.blank?
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create name: "admin",
+            password: "admin123",
+            email: "admin@situsali.com",
+            role: :admin,
+            confirmed_at: DateTime.now.to_formatted_s(:db)
